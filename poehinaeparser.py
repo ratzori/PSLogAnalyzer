7b6228c89b0ba27e477bbff1bb4135a53537136e
Tommi Tauriainen <ratzori@users.noreply.github.com>
2017-02-03 20:18:42 +0200

    Added more items to parse

--- poehinaeparser.py
+++ poehinaeparser.py
@@ -4,7 +4,7 @@ import re
 
 # Input handlers
 
-#PokerStars Hand #xxxx...:  Hold'em No Limit ($xx.xx/$xx.xx USD) - YYYY/MM/DD HH:MM:SS ET
+#PokerStars Hand #xxxx...:  Hold'em No Limit ($xx.xx/$xx.xx USD) - YYYY/MM/DD HH:MM:SS TZ
 def hand( data ):
     hand_number = data.group(1)
     game_type = data.group(2)
@@ -55,11 +55,68 @@ def big_blind( data ):
 
     return
 
+# *** HOLE CARDS ***
+def hole_cards( data ):
+
+    # Do something
+    print("Hole cards")
+
+    return
+
+# Dealt to nick [xx xx]
+def dealt( data ):
+
+    player = data.group(1)
+    card1 = data.group(2)
+    card2 = data.group(3)
+
+    # Do something
+    print("Dealt cards", player, card1, card2)
+
+    return
+
+# nick: folds
+def folds( data ):
+
+    player = data.group(1)
+
+    # Do something
+    print("Folds", player)
+
+    return
+
+# nick: checks
+def checks( data ):
+
+    player = data.group(1)
+
+    # Do something
+    print("Checks", player)
+
+    return
+
+# *** FLOP CARDS *** [xx yy zz]
+def flop_cards( data ):
+
+    card1 = data.group(1)
+    card2 = data.group(2)
+    card3 = data.group(3)
+
+    # Do something
+    print("Flop cards", card1, card2, card3)
+
+    return
+
 # Compile Regex patterns
 hand_re = re.compile(r'^PokerStars Hand #([0-9]+): (.+) \(\$([0-9]+\.[0-9]+)\/\$([0-9]+\.[0-9]+) (\w+)\) - ([0-9])+\/([0-9])+\/([0-9])+ ([0-9])+:([0-9])+:([0-9])+([0-9])+ (\w+)')
 table_status_re = re.compile(r'^Seat ([0-9]): (\w+) \(\$([0-9]+\.[0-9]+) in chips\)')
 small_blind_re = re.compile(r'^(\w+): posts small blind \$([0-9]+\.[0-9]+)')
 big_blind_re = re.compile(r'^(\w+): posts big blind \$([0-9]+\.[0-9]+)')
+hole_cards_re = re.compile(r'^\*\*\* HOLE CARDS \*\*\*')
+dealt_re = re.compile(r'^Dealt to (\w+) \[(\w+) (\w+)\]')
+folds_re = re.compile(r'^(\w+): folds')
+checks_re = re.compile(r'^(\w+): checks')
+flop_cards_re = re.compile(r'^\*\*\* FLOP \*\*\* \[(\w+) (\w+) (\w+)\]')
 
 # Main parser loop
 for line in sys.stdin:
@@ -86,5 +143,35 @@ for line in sys.stdin:
 
     if ( big_blind_m ):
         big_blind( big_blind_m )
+        continue
+
+    hole_cards_m = hole_cards_re.match(line)
+
+    if ( hole_cards_m ):
+        hole_cards( hole_cards_m )
+        continue
+
+    dealt_m = dealt_re.match(line)
+
+    if ( dealt_m ):
+        dealt( dealt_m )
+        continue
+
+    folds_m = folds_re.match(line)
+
+    if ( folds_m ):
+        folds( folds_m )
+        continue
+
+    checks_m = checks_re.match(line)
+
+    if ( checks_m ):
+        checks( checks_m )
+        continue
+
+    flop_cards_m = flop_cards_re.match(line)
+
+    if ( flop_cards_m ):
+        flop_cards( flop_cards_m )
         continue
 
